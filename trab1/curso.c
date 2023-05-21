cff5d00c896c58e4c0d5f708d8ff87703d23d380
Willians-S-S <09willians@gmail.com>
2023-05-21 00:12:55 -0300

    Adição. Foram adicionadas algumas funções de imprimir 0.2

--- trab1/curso.c
+++ trab1/curso.c
@@ -102,9 +102,26 @@ void imprimirCursosQtdB(Curso *raiz, int qtdB){
 
 void imprimirDisciplina(Disciplina *raiz){
     if(raiz){
-        imprimirDisciplina(raiz->esq);
         printf("CodigoD: %d\nNome Dis: %s\nBlocos da disciplina: %d\nCarga Horaria: %d\n\n", raiz->codD, raiz->nome, raiz->qtdBDisciplina, raiz->cargHor);
-        imprimirDisciplina(raiz->dir);
+    }
+}
+
+void imprimirDadosDisc(Disciplina *raiz){
+    if(raiz){
+        imprimirDadosDisc(raiz->esq);
+        imprimirDisciplina(raiz);
+        imprimirDadosDisc(raiz->dir);
+    }
+}
+
+void imprimirDisciplinaCod(Disciplina *raiz, int cod){
+    if(raiz){
+        if(raiz->codD == cod)
+            imprimirDisciplina(raiz);
+        else if(cod < raiz->codD)
+            imprimirDisciplinaCod(raiz->esq, cod);
+        else
+            imprimirDisciplinaCod(raiz->dir, cod);
     }
 }
 
